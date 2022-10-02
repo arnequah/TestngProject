94cdd9ec0845630576699d2c1e2d6c330bae2be8
arnequah <arne.quah@dbresults.com.au>
2022-10-02 15:07:33 +1100

    TestngProject

---
+++ src/test/CardTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class CardTest extends BaseClass{
+	
+	@Test(groups= {"sanity"}, enabled = true, description = "This test will validate the CC functionality")
+	public void CCTest() {
+		
+		System.out.println("Inside CC Test");
+	}
+
+}
---
+++ src/test/LoanTest.java
@@ -0,0 +1,27 @@
+package test;
+
+import org.testng.annotations.Test;
+
+@Test(groups= {"regression"})
+
+public class LoanTest extends BaseClass{
+	
+	@Test(priority=1)
+	public void HomeLoanTest() {
+		
+		System.out.println("Inside Home Loan Test");
+	}
+	
+	@Test(enabled=true)
+	public void PersonalLoanTest() {
+		
+		System.out.println("Inside Personal Loan Test");
+	}
+	
+	@Test(priority=0)
+	public void PersonalLoanTest1() {
+		
+		System.out.println("Inside Personal Loan Test 1");
+	}
+
+}
